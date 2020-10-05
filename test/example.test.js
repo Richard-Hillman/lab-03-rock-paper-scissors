3d59f80c09d3938147b7ec179f6ae1b1e704cf94
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-10-04 19:53:33 -0700

    made changes on weekend for full points, has full functionality, has css styling, has reset button, has backgrnd img, has radio hidden, has color scheme.

--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,90 @@
 // IMPORT MODULES under test here:
-import { randomThrow } from '../utilities.js';
+import { randomThrow, doesUserWin } from '../utilities.js';
 
 // import { example } from '../example.js';
 
 const test = QUnit.test;
+
+
+//  SCISSORS TESTS ==========================================================================================================
+
+test('if computer input scissors and user input is rock return win', (expect) => {
+    const userInput = 'rock';
+    const compInput = 'scissors';
+
+    const expected = 'You Win';
+    const actual = doesUserWin(userInput, compInput);
+
+    expect.equal(actual, expected);
+});
+
+test('if computer input scissors and user input is paper return lose', (expect) => {
+    const userInput = 'paper';
+    const compInput = 'scissors';
+
+    const expected = 'You Lose';
+    const actual = doesUserWin(userInput, compInput);
+
+    expect.equal(actual, expected);
+});
+
+// PAPER TESTS =============================================================================================================
+
+test('if computer input paper and user input is scissors return win', (expect) => {
+    const userInput = 'scissors';
+    const compInput = 'paper';
+
+    const expected = 'You Win';
+    const actual = doesUserWin(userInput, compInput);
+
+    expect.equal(actual, expected);
+});
+
+test('if computer input paper and user input is rock return lose', (expect) => {
+    const userInput = 'rock';
+    const compInput = 'paper';
+
+    const expected = 'You Lose';
+    const actual = doesUserWin(userInput, compInput);
+
+    expect.equal(actual, expected);
+});
+
+// ROCK TESTS =============================================================================================================
+
+test('if computer input is rock and user input is paper return win', (expect) => {
+    const userInput = 'paper';
+    const compInput = 'rock';
+
+    const expected = 'You Win';
+    const actual = doesUserWin(userInput, compInput);
+
+    expect.equal(actual, expected);
+});
+
+test('if computer input is rock and user input is scissors return lose', (expect) => {
+    const userInput = 'scissors';
+    const compInput = 'rock';
+
+    const expected = 'You Lose';
+    const actual = doesUserWin(userInput, compInput);
+
+    expect.equal(actual, expected);
+});
+
+// DRAW TEST ===============================================================================================================
+
+test('if computer input is the same as user input we want it to return tie', (expect) => {
+    const userInput = 'rock';
+    const compInput = 'rock';
+
+    const expected = 'Tie';
+    const actual = doesUserWin(userInput, compInput);
+
+    expect.equal(actual, expected);
+});
+
+// RANDOM NUMBER ROCK PAPER SCISSORS FUNCTION ================================================================================
 
 test('should randomly return rock, paper, scissors', (expect) => {
     //Arrange
